6a53993558b1f8c55ee9c70aecf07f5613445855
Diya <160126722+Diya-14@users.noreply.github.com>
2024-10-17 23:03:01 -0700

    make a website of tailwand css

---
+++ my-nextjs-project/src/pages/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <p className="text-lg">Loading...</p>
+      </div>
+    );
+  }
+  
+  export default Loading;
+  
